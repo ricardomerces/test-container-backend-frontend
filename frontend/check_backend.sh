b1a2704d31095155864927057febf45c0c06023f
ricardo.merces <ricardo.merces@gmail.com>
2025-06-27 08:00:09 -0300

    change message

--- frontend/check_backend.sh
+++ frontend/check_backend.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 while true; do
   echo "$(date): Checking backend..."
-  # Example: curl -Is http://backend:8080 || echo "Failed to reach backend"
-  curl -Is "${BACKEND_URL}" || echo "Failed to reach backend"
+  # Example: curl -Is http://backend:8080 || echo "Falha de conexão ao BACKEND"
+  curl -I "${BACKEND_URL}" || echo "Falha de conexão ao BACKEND"
   echo
   sleep 5
 done
