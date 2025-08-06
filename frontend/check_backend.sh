236a8c21c21c86b03693a752a11d0658059a7024
ricardo.merces <ricardo.merces@gmail.com>
2025-08-06 19:29:30 -0300

    refactor]

--- frontend/check_backend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-while true; do
-  echo "$(date): Checking backend..."
-  # Example: curl -Is http://backend:8080 || echo "Falha de conexão ao BACKEND"
-  curl -I "${BACKEND_URL}"
-  echo
-  sleep 5
-done
